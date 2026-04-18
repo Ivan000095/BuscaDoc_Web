37b9009c8d65ecdb8c14bae3e9a217d85a04a738
Jose Ivan Lanestosa Pelayo <ivanlanestosa8@gmail.com>
2026-04-18 16:59:25 -0600

    Fix: Instalar postgresql-client y configurar dump path

--- config/database.php
+++ config/database.php
@@ -96,7 +96,12 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'prefer',
+            'dump' => [
+                'dump_binary_path' => '/usr/bin/', 
+            ],
         ],
+
+        
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
